1f5119b9a78cec6686dfd79dab2d6d800ce9eb3b
Eagle <eagsmart@gmail.com>
2019-10-25 15:42:50 +0200

    Changed index.php and index2.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <!-- 
+
+    - git je distribuirani, decentralizovani version control system
+    - snapshot -- > commit
+    - staging area je samo add command
+
+    git init - inicijalizacija repozitorijuma u nekom folderu (kreacija .git fajla)
+    git add <file> - dodaje fajlove u staging area (index) onda su spremni za commit
+    git status - drvo, sto ima u staging area, razlike izmedju working tree i staging area (index)
+    git commit - stavlja sve iz staging area u repozitorijum lokalni
+
+    git push - iz lokalnog repozitorijuma u remote
+    git pull - iz remote zadnju verziju
+
+    git clone - downloaduje iz remote u folder
+
+    git add *.html - svi html
+    git add . - svi
+
+
+    -->
+
+    <p> Paragraf </p>
+
+    
+
+</body>
+</html>
